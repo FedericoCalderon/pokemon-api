adf227b3c83d87d6d024a0d7d409691fe40e47c6
FedericoCalderon <fede4872@gmail.com>
2022-11-24 08:43:14 -0300

    delete console.log

--- src/db/connection.js
+++ src/db/connection.js
@@ -16,12 +16,9 @@ export let query = function (sql, values) {
   return new Promise((resolve, reject) => {
     pool.getConnection(function (err, connection) {
       if (err) {
-        console.log('\n\n!!!!!!!!!!!conection fail!!!!!!!!!\n\n')
         reject(err)
       } else {
-        console.log('\n\n!!!!!!!!!!!conection success!!!!!!!!!\n\n')
         connection.query(sql, values, (err, rows) => {
-
           if (err) {
             errorMessage(err.code, err.message, err.name);
             reject(err);
