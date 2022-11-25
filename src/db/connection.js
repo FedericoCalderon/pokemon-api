e16e655619badb9c89b412405dff6db8c6f96a24
FedericoCalderon <fede4872@gmail.com>
2022-11-25 14:13:34 -0300

    fix DB_USER env

--- src/db/connection.js
+++ src/db/connection.js
@@ -5,7 +5,7 @@ config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: { rejectUnauthorized: true }
@@ -16,6 +16,7 @@ export let query = function (sql, values) {
   return new Promise((resolve, reject) => {
     pool.getConnection(function (err, connection) {
       if (err) {
+        console.log("ENTRA A ERR")
         reject(err)
       } else {
         connection.query(sql, values, (err, rows) => {
